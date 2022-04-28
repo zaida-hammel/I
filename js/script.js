0724cda26d05b0993daaac88cf38e3383e3fafb6
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-28 02:56:38 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,68 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ava Venturino
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment4-HTML/sw.js", {
+    scope: "/ICS2O-Assignment4-HTML/",
+  });
+}
+
+/**
+ * Input
+ */
+function pizzaOrder() {
+  var price = 0;
+  var top = 0;
+  var total = 0;
+  var tax = 0;
+
+  var largePizza = document.getElementById("largePizza");
+  var extraLargePizza = document.getElementById("extraLargePizza");
+
+  if (largePizza.checked == true) {
+    price = 6.00;
+    document.getElementById("s_result").innerHTML = "Large";
+  } else if (extraLargePizza.checked == true) {
+    price = 10.00;
+    document.getElementById("s_result").innerHTML = "Extra large";
+  }
+
+  document.getElementById("p_result").innerHTML = "$" + price;
+
+  var topping1 = document.getElementById("topping1");
+  var topping2 = document.getElementById("topping2");
+  var topping3 = document.getElementById("topping3");
+  var topping4 = document.getElementById("topping4");
+
+  if (topping1.checked == true) {
+    topping = 1.00;
+    document.getElementById("t_options").innerHTML = "1 Topping";
+  }
+
+  if (topping2.checked == true) {
+    topping = 1.75;
+    document.getElementById("t_options").innerHTML = "2 Toppings";
+  }
+
+  if (topping3.checked == true) {
+    topping = 2.5;
+    document.getElementById("t_options").innerHTML = "3 Toppings";
+  }
+
+  if (topping4.checked == true) {
+    topping = 3.35;
+    document.getElementById("t_options").innerHTML = "4 Toppings";
+  }
+
+  document.getElementById("t_result").innerHTML = "$" + top;
+
+  tax = (price + topping) * 0.13;
+  total = tax + (price + topping);
+
+  document.getElementById("answers").innerHTML =
+    "Your Current Total is $ " + total.toFixed(2);
 }
